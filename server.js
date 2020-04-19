9a769ff57b1548cde2a42bb1eba189f9b7270d06
Abdallah <aabedraba@gmail.com>
2020-04-19 10:29:32 +0200

    Working with HTTP methods

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-import express from "express";
+const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', (req, res) => {
-    res.send('Hola')
-});
+app.use(router);
+
+router.get('/message', (req, res) => {
+    res.send("Message list");
+})
+
+router.post('/message', (req, res) => {
+    res.send("Message added");
+})
+// app.use('/', (req, res) => {
+//     res.send('Hola')
+// });
 
 app.listen(3000);
 console.log("Listening in http://localhost:3000")
