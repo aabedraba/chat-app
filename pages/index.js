93c48abe4c265978ce0bd9f29c3364b6460caeec
Abdallah <aabedraba@gmail.com>
2020-04-22 23:45:12 +0200

    Fetching messages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
-export default function Index(){
-    return(
-        <h1>Hello chat</h1>
+import fetch from 'node-fetch';
+
+export default function Index(props) {
+    return (
+        <div>
+            <h1>Hello chat</h1>
+            <ul>
+                {props.messages.body
+                    .map(message => <li key={message.id}>{message.message}</li>)}
+            </ul>
+            
+        </div>
     )
+}
+
+export async function getStaticProps(){
+    const res = await fetch('http://localhost:3001/message');
+    const messages = await res.json();
+
+    return {
+        props: {
+            messages
+        }
+    }
 }
