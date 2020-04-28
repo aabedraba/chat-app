a9114f75b971809457d872113ec2b49d1eb38c6e
Abdallah <aabedraba@gmail.com>
2020-04-28 18:44:17 +0200

    Chat re-implemented

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,25 +1,28 @@
 import Layout from '../../components/Layout'
 import Messages from '../../components/Messages'
-import { getChatId, fetchMessage, storeMessage } from '../../utils'
+import { getChatId, fetchMessage, storeMessage, getUserName } from '../../store'
 import { useState } from 'react'
 
 function Chat(props) {
-    const userId = props.userId;
-    const chatId = props.chatId;
+    const userName = props.userName
+    const userId = props.userId
+    const chatId = props.chatId
     const [messageList, setMessageList] = useState(props.messageList)
     const [messageCount, setMessageCount] = useState(0)
     const [newMessage, setNewMesage] = useState('');
-    
+
     const handleChange = (e) => { setNewMesage(e.target.value) }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        setMessageList([...messageList, 
-            { id: messageCount,
-              message: newMessage  
-            }])
+        storeMessage({ message: newMessage, chatId, userId })
+        setMessageList(() => [...messageList,
+        {
+            id: messageCount,
+            message: newMessage,
+            userName: userName
+        }])
         setMessageCount(messageCount + 1)
-        storeMessage({message: newMessage, chatId, userId})
         setNewMesage('')
     }
 
@@ -28,23 +31,44 @@ function Chat(props) {
             <Messages messageList={messageList} />
             <form onSubmit={handleSubmit}>
                 <input type="text" onChange={handleChange} value={newMessage}></input>
-                <input type="submit" value="Submit"></input>
+                <button type="submit" value="Submit">Send</button>
             </form>
+            <style jsx>{`
+                form {
+                    background-color: #fff;
+                    display: flex;
+                    height: 32px;
+                    position: fixed;
+                    bottom: 0;
+                    left: 0;
+                    right: 0;
+                    width: 100%;
+                }
+                form input {
+                    width: 90%;
+                }
+                form button {
+                    width: 10%;
+                }
+            `}</style>
         </Layout>
     )
 }
 
 Chat.getInitialProps = async function (context) {
     const userId = context.query.id
+    const userName = await getUserName(userId)
     const chatId = await getChatId(userId)
     const messageData = await fetchMessage(await chatId);
     const messageList = await messageData.body.map(element => {
         return {
             id: element._id,
-            message: element.message
+            message: element.message,
+            userName: "Abdallah"
         }
     })
     return {
+        userName,
         userId,
         chatId,
         messageList
