efa959b42416222d4df92252655492edb91ea4c3
Abdallah <aabedraba@gmail.com>
2020-04-28 18:44:43 +0200

    New heroku link

--- store/index.js
+++ store/index.js
@@ -1,28 +1,30 @@
+import fetch from 'isomorphic-unfetch'
+
 async function fetchMessage(chatId) {
-    return fetch(`https://next-chat-app-aabedraba.herokuapp.com/message?chat=${chatId}`)
+    return fetch(`https://nodejs-chat-backend.herokuapp.com/message?chat=${chatId}`)
         .then(res => res.json())
 }
 
 async function getChatId(userId) {
-    return await fetch(`https://next-chat-app-aabedraba.herokuapp.com/chat/${userId}`)
+    return await fetch(`https://nodejs-chat-backend.herokuapp.com/chat/${userId}`)
         .then(res => res.json())
         .then(data => data.body[0]._id)
 }
 
 async function getUserName(userId) {
-    const res = await fetch('https://next-chat-app-aabedraba.herokuapp.com/user')
+    const res = await fetch('https://nodejs-chat-backend.herokuapp.com/user')
     const data = await res.json()
     const user = data.body.find(element => element._id === userId)
     return user.name;
 }
 
-async function storeMessage({message, chatId, userId}) {
+function storeMessage({message, chatId, userId}) {
     const messageToStore = {
         message, 
         chat: chatId,
         user: userId
     }
-    const res = await fetch('https://next-chat-app-aabedraba.herokuapp.com/message', {
+    fetch('https://nodejs-chat-backend.herokuapp.com/message', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
