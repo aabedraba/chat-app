659fd7c296f9b55d6e5145da7889342ea975ccbb
Abdallah <aabedraba@gmail.com>
2020-04-28 20:55:57 +0200

    Abstract Form component

---
+++ components/Form.js
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { storeMessage } from '../store' 
+
+export default function Form(props) {
+    const chatId = props.chatId
+    const userId = props.userId
+
+    const [newMessage, setNewMessage] = useState('')
+    const [messageCount, setMessageCount] = useState(0)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        storeMessage({ message: newMessage, chatId, userId })
+        setMessageCount(messageCount + 1)
+        setNewMessage('')
+    }
+
+    const handleChange = (e) => { setNewMessage(e.target.value) }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={handleChange} value={newMessage}></input>
+                <button type="submit" value="Submit">Send</button>
+            </form>
+            <style jsx>{`
+                form {
+                    background-color: #fff;
+                    display: flex;
+                    height: 32px;
+                    position: fixed;
+                    bottom: 0;
+                    left: 0;
+                    right: 0;
+                    width: 100%;
+                    }
+                form input {
+                    width: 90%;
+                }
+                form button {
+                    width: 10%;
+                }
+            `}</style>
+        </div>
+    )
+}
+
--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,56 +1,13 @@
 import Layout from '../../components/Layout'
 import Messages from '../../components/Messages'
-import { getChatId, fetchMessage, storeMessage, getUserName } from '../../store'
-import { useState } from 'react'
+import Form from '../../components/Form'
+import { getChatId, fetchMessage, getUserName } from '../../store'
 
 function Chat(props) {
-    const userName = props.userName
-    const userId = props.userId
-    const chatId = props.chatId
-    const [messageList, setMessageList] = useState(props.messageList)
-    const [messageCount, setMessageCount] = useState(0)
-    const [newMessage, setNewMesage] = useState('');
-
-    const handleChange = (e) => { setNewMesage(e.target.value) }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        storeMessage({ message: newMessage, chatId, userId })
-        setMessageList(() => [...messageList,
-        {
-            id: messageCount,
-            message: newMessage,
-            userName: userName
-        }])
-        setMessageCount(messageCount + 1)
-        setNewMesage('')
-    }
-
     return (
         <Layout >
-            <Messages messageList={messageList} />
-            <form onSubmit={handleSubmit}>
-                <input type="text" onChange={handleChange} value={newMessage}></input>
-                <button type="submit" value="Submit">Send</button>
-            </form>
-            <style jsx>{`
-                form {
-                    background-color: #fff;
-                    display: flex;
-                    height: 32px;
-                    position: fixed;
-                    bottom: 0;
-                    left: 0;
-                    right: 0;
-                    width: 100%;
-                }
-                form input {
-                    width: 90%;
-                }
-                form button {
-                    width: 10%;
-                }
-            `}</style>
+            <Messages messageList={props.messageList} />
+            <Form userName={props.userName} userId={props.userId} chatId={props.chatId} />
         </Layout>
     )
 }
@@ -64,7 +21,7 @@ Chat.getInitialProps = async function (context) {
         return {
             id: element._id,
             message: element.message,
-            userName: "Abdallah"
+            userName: element.user.name
         }
     })
     return {
