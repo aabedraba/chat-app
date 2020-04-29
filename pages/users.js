ba8a486ad88231b5cd4de06f1a10dacaa65a0770
Abdallah <aabedraba@gmail.com>
2020-04-29 11:07:35 +0200

    Fetching by Users added

--- pages/users.js
+++ pages/users.js
@@ -1,28 +1,28 @@
 import Layout from '../components/Layout';
 import Button from '@material-ui/core/Button';
 import Link from 'next/link';
-import useSWR from 'swr'
+import { getUsers } from '../lib/store'
+import { useState, useEffect } from 'react'
 
-async function fetcher (url) {
-    return fetch(url).then(res => res.json())
+function UserLinks(props) {
+    return (
+        props.users.map(user =>
+            <Link href={{ pathname: '/chat', query: { user: user._id } }} key={user._id}>
+                <Button variant="contained">{user.name}</Button>
+            </Link>)
+    )
 }
-
 function Users() {
-    const {data, error} = useSWR('http://localhost:3001/user', fetcher)
+    const [users, setUsers] = useState([])
 
-    if (error) return <div>Error loading</div>
+    useEffect(() => {
+        getUsers().then(users => setUsers(users.body))
+    }, [])
 
     return (
         <Layout>
-
             <div>
-                {data 
-                ? data.body.map(user =>
-                    <Link href={{ pathname: '/chat', query: { user: user._id } }} key={user._id}>
-                        <Button variant="contained">{user.name}</Button>
-                    </Link>)
-                : 'Loading...'
-                }
+                <UserLinks users={users} />
             </div>
             <style jsx>{`
                 div {
